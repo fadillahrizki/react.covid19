53f7c0b0c4c5fb889e09d551a271b6007bc90518
unknown <fadillahrizki16@gmail.com>
2020-04-06 23:27:16 +0700

    Updates

---
+++ precache-manifest.648f3ba5fe56fb99c17e1ac2daada624.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80efaa02f11ae029bca426b6a2d1a51b",
+    "url": "/react.covid19/index.html"
+  },
+  {
+    "revision": "c30b73bc18781953d983",
+    "url": "/react.covid19/static/css/main.0cbb4c39.chunk.css"
+  },
+  {
+    "revision": "f8f1e5236c110bf356b1",
+    "url": "/react.covid19/static/js/2.5a68ea28.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react.covid19/static/js/2.5a68ea28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30b73bc18781953d983",
+    "url": "/react.covid19/static/js/main.16aacd64.chunk.js"
+  },
+  {
+    "revision": "6e81feafda9a8e300205",
+    "url": "/react.covid19/static/js/runtime-main.b1bd75a3.js"
+  },
+  {
+    "revision": "26ecea43950733d8de918ddb4457fe3d",
+    "url": "/react.covid19/static/media/iconmouse.26ecea43.svg"
+  },
+  {
+    "revision": "c26d9accf3c09d4da2c0d1f86eecbe84",
+    "url": "/react.covid19/static/media/owner.c26d9acc.jpeg"
+  },
+  {
+    "revision": "6ac82756c4ec444df5c3c310d4d217d7",
+    "url": "/react.covid19/static/media/variant.6ac82756.svg"
+  }
+]);
